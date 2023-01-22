235afbf0f35b52d799dad2f7497397e20f283d34
aanisar <55286102+aanisar@users.noreply.github.com>
2023-01-22 16:39:16 -0500

    I have set up the email and google authentication. I haven't touch the design part. On LogInActivity, it added the two buttons for logging into with email and with google account.

---
+++ app/src/main/java/com/lh1110642/gymgenie/LoginActivity.kt
@@ -0,0 +1,58 @@
+package com.lh1110642.gymgenie
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.lh1110642.gymgenie.databinding.ActivityLoginBinding
+import com.lh1110642.gymgenie.databinding.ActivityMainBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+            Toast.makeText(this, "Unsuccessful", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+}
--- app/src/main/java/com/lh1110642/gymgenie/MainActivity.kt
+++ app/src/main/java/com/lh1110642/gymgenie/MainActivity.kt
@@ -2,10 +2,14 @@ package com.lh1110642.gymgenie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lh1110642.gymgenie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
