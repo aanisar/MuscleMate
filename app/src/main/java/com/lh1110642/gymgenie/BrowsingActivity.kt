011e623a86fe1d799332f4a6504df94daa46c12e
aanisar <55286102+aanisar@users.noreply.github.com>
2023-02-25 22:33:09 -0500

    just pushing new changes that i made so far

---
+++ app/src/main/java/com/lh1110642/gymgenie/BrowsingActivity.kt
@@ -0,0 +1,11 @@
+package com.lh1110642.gymgenie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BrowsingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browsing)
+    }
+}
