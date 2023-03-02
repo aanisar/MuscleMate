e35c1c852c2daa2c0a15d53935c1be192b873119
aanisar <55286102+aanisar@users.noreply.github.com>
2023-03-01 22:21:01 -0500

    just fixed some minor issues

--- app/src/main/java/com/lh1110642/gymgenie/BrowsingActivity.kt
+++ app/src/main/java/com/lh1110642/gymgenie/BrowsingActivity.kt
@@ -26,122 +26,105 @@ class BrowsingActivity : AppCompatActivity() {
 //            makeText(baseContext, "Abs is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "abdominals"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.leftShoulder.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Shoulder is been clicked",
 //                Toast.LENGTH_SHORT).show()
 
             muscle = "neck"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.rightShoulder.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Shoulder is been clicked",
 //                Toast.LENGTH_SHORT).show()
 
             muscle = "neck"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.rightTrap.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Trap is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "traps"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.leftTrap.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Trap is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "traps"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.leftChest.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Chest is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "chest"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.rightChest.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Chest is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "chest"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.frontLeftForearm.setOnClickListener{
 //            Toast.makeText(baseContext, "Front Left Forearm is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "forearms"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.frontRightForearm.setOnClickListener{
 //            Toast.makeText(baseContext, "Front Right Forearm is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "forearms"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.rightBicep.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Bicep is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "biceps"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.leftBicep.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Bicep is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "biceps"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.leftOblique.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Oblique is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "abdominals"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.rightOblique.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Oblique is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "abdominals"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.rightQuad.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Quad is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "quadriceps"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.leftQuad.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Quad is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "quadriceps"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.leftCalf.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Calf is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "calves"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.rightCalf.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Calf is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "calves"
-            startActivity(Intent(this,ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.swap.setOnClickListener{
             startActivity(Intent(this, BrowsingBackActivity::class.java))
--- app/src/main/java/com/lh1110642/gymgenie/BrowsingBackActivity.kt
+++ app/src/main/java/com/lh1110642/gymgenie/BrowsingBackActivity.kt
@@ -20,29 +20,26 @@ class BrowsingBackActivity : AppCompatActivity() {
 //            Toast.makeText(baseContext, "Back Trap is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "traps"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
+            startActivity(Intent(this, ExerciseListActivity::class.java))
         }
 
         binding.trapMidBack.setOnClickListener{
 //            Toast.makeText(baseContext, "Trap Mid-Back is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "lowerback middleback"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.lowerBack.setOnClickListener{
 //            Toast.makeText(baseContext, "Lower Back is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "lowerback middleback"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.leftLat.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Lat is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "lats"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.rightLat.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Lat is been clicked",
@@ -50,92 +47,79 @@ class BrowsingBackActivity : AppCompatActivity() {
             //            Toast.makeText(baseContext, "Left Lat is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "lats"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.leftCheek.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Cheek is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "glutes"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.rightCheek.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Cheek is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "glutes"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.backLeftShoulder.setOnClickListener{
 //            Toast.makeText(baseContext, "Back Left Shoulder is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "neck"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.backRightShoulder.setOnClickListener{
 //            Toast.makeText(baseContext, "Back Right Shoulder is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "neck"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.rightTricep.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Tricep is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "triceps"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.leftTricep.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Tricep is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "triceps"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.backLeftForearm.setOnClickListener{
 //            Toast.makeText(baseContext, "Back Left Forearm is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "forearms"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.backRightForearm.setOnClickListener{
 //            Toast.makeText(baseContext, "Back Right Forearm is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "forearms"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.leftHamstring.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Hamstring is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "hamstrings"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.rightHamstring.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Hamstring is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "hamstrings"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.leftCalf.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Calf is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "calves"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.rightCalf.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Calf is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "calves"
-            startActivity(Intent(this, ActivityExerciseListBinding::class.java))
-        }
+            startActivity(Intent(this, ExerciseListActivity::class.java))        }
 
         binding.swap.setOnClickListener{
             startActivity(Intent(this, OtherFiltersActivity::class.java))
