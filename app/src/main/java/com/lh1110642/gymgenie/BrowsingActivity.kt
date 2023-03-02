d2a2e0bbe15ffcaf4711b09edc15565fa1532012
Mason8766 <77406208+Mason8766@users.noreply.github.com>
2023-03-02 11:51:07 -0500

    Added api based filtering

--- app/src/main/java/com/lh1110642/gymgenie/BrowsingActivity.kt
+++ app/src/main/java/com/lh1110642/gymgenie/BrowsingActivity.kt
@@ -3,10 +3,14 @@ package com.lh1110642.gymgenie
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
 import com.lh1110642.gymgenie.databinding.ActivityBrowsingBinding
 import com.lh1110642.gymgenie.databinding.ActivityExerciseListBinding
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.util.ArrayList
 
 
@@ -26,109 +30,148 @@ class BrowsingActivity : AppCompatActivity() {
 //            makeText(baseContext, "Abs is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "abdominals"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.leftShoulder.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Shoulder is been clicked",
 //                Toast.LENGTH_SHORT).show()
 
             muscle = "neck"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.rightShoulder.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Shoulder is been clicked",
 //                Toast.LENGTH_SHORT).show()
 
             muscle = "neck"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.rightTrap.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Trap is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "traps"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.leftTrap.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Trap is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "traps"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.leftChest.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Chest is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "chest"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.rightChest.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Chest is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "chest"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.frontLeftForearm.setOnClickListener{
 //            Toast.makeText(baseContext, "Front Left Forearm is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "forearms"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.frontRightForearm.setOnClickListener{
 //            Toast.makeText(baseContext, "Front Right Forearm is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "forearms"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.rightBicep.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Bicep is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "biceps"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.leftBicep.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Bicep is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "biceps"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.leftOblique.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Oblique is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "abdominals"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.rightOblique.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Oblique is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "abdominals"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.rightQuad.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Quad is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "quadriceps"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.leftQuad.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Quad is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "quadriceps"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.leftCalf.setOnClickListener{
 //            Toast.makeText(baseContext, "Left Calf is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "calves"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+             startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))          }
 
         binding.rightCalf.setOnClickListener{
 //            Toast.makeText(baseContext, "Right Calf is been clicked",
 //                Toast.LENGTH_SHORT).show()
             muscle = "calves"
-            startActivity(Intent(this, ExerciseListActivity::class.java))        }
+            Log.i("Muscle Picked: ", muscle)
+            
+            
+            startActivity(Intent(this, ExerciseListActivity::class.java).putExtra("muscle", muscle).putExtra("difficulty","").putExtra("type", ""))        
+        }
 
         binding.swap.setOnClickListener{
             startActivity(Intent(this, BrowsingBackActivity::class.java))
         }
     }
+
+    
 
 }
--- app/src/main/java/com/lh1110642/gymgenie/ExerciseListActivity.kt
+++ app/src/main/java/com/lh1110642/gymgenie/ExerciseListActivity.kt
@@ -1,19 +1,138 @@
 package com.lh1110642.gymgenie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import com.lh1110642.gymgenie.databinding.ActivityExerciseListBinding
-import java.util.ArrayList
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class ExerciseListActivity : AppCompatActivity() {
     private lateinit var adapterExercise: Adapter
     var exerciseList: ArrayList<Exercise> = ArrayList()
+
+    var muscle = ""
+    var diff = ""
+    var type = ""
+
+    val listExercise = arrayOfNulls<Exercise>(10)
+
     private lateinit var binding: ActivityExerciseListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        muscle = intent.getStringExtra("muscle").toString()
+        diff = intent.getStringExtra("difficulty").toString()
+        type = intent.getStringExtra("type").toString()
+        apiCall()
+
+        //listExercise containts the api call of all excercises
+    }
+
+    fun apiCall(){
+
+        var  APIMod = ""
+        //  var muscle = ""
+//        var diff = ""
+//        var type = ""
+
+        //Filters
+        if (muscle != "")
+        {
+            if (APIMod == "")
+                APIMod = "?muscle="+muscle
+            else{
+                APIMod += "&muscle="+muscle
+            }
+        }
+        if (diff != "")
+        {
+            if (APIMod == "")
+                APIMod = "?difficulty="+diff
+            else{
+                APIMod += "&difficulty="+diff
+            }
+        }
+        if (type != "")
+        {
+            if (APIMod == "")
+                APIMod = "?type="+type
+            else{
+                APIMod += "&type="+type
+            }
+        }
+
+        var url = "https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises" + APIMod;
+        //"https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=biceps
+        Thread { //Thread that runs networking in aysnc
+
+            //Code that calls the api, with URI and link
+            val client = OkHttpClient()
+            val request = Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("X-RapidAPI-Key", "088a03c490mshbabdd39f3567b25p1b021ajsnd500a6f71c98")
+                .addHeader("X-RapidAPI-Host", "exercises-by-api-ninjas.p.rapidapi.com")
+                .build()
+
+            try {
+
+                //Calls API, then converts into a Gson() into a string
+                val response = client.newCall(request).execute()
+                val result: String = Gson().toJson(response.body!!.string())
+
+                val x = result // set x to the result string, for substringing
+
+                //split string delimited by bracket
+                val stringarray =
+                    x.split("},").toMutableList()
+
+
+                for (i in stringarray.indices) { //Corrects each Exercise by adding bracket
+                    //prints the tokens
+                    stringarray[i] += "}"
+                }
+
+                //Corrects the leading and trailing char's in the first and last member of the array
+                stringarray[0] = stringarray[0].substring(1, stringarray[0].length)
+                stringarray[stringarray.size - 1] = stringarray[stringarray.size - 1].substring(0, stringarray[stringarray.size - 1].length - 2)
+
+
+               // val listExercise = arrayOfNulls<Exercise>(10)
+                for (i in stringarray.indices) {
+
+                    //Replaces the def of each exercise category, with a deliminator
+                    var str = stringarray[i]
+                    str = str.replace("\\\"name\\\":", "@!@")
+                    str = str.replace("\\\"type\\\":", "@!@")
+                    str = str.replace("\\\"muscle\\\":", "@!@")
+                    str = str.replace("\\\"equipment\\\":", "@!@")
+                    str = str.replace("\\\"difficulty\\\":", "@!@")
+                    str = str.replace("\\\"instructions\\\":", "@!@")
+                    str.replace("\"", "")
+                    val strArr = str.split("@!@") //split each category into 1 list
+
+                    //Creates a exercise, then inserts it into a list
+                    val exercise = Exercise(strArr[1].substring(3, strArr[1].length - 4), strArr[2].substring(3, strArr[2].length - 4), strArr[3].substring(3, strArr[3].length - 4), strArr[4].substring(3, strArr[4].length - 4), strArr[5].substring(3, strArr[5].length - 4), strArr[6].substring(3, strArr[6].length - 4))
+                    listExercise[i] = exercise
+                }
+
+                //Just displays the list for viewing purpose, NOT NEEDED
+                println(listExercise[3]!!.excerciseToString())
+                for (i in stringarray.indices) {
+                    //prints the tokens
+                    println(listExercise[i]!!.getName())
+                    Log.i("Name: ", listExercise[i]!!.getName())
+                    println("_________________________")
+                }
+                Log.i("Name: ", listExercise[3]!!.excerciseToString())
+
+            }catch (err:Error) {//if call fails
+                Log.i("error","ERRRRRRRRRRRR")
+            }
+        }.start()
     }
 }
